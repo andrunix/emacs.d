b416b92d6f4811a450090b5a0d7a47a5d38b8ab4
= <andrew@xoso.io>
2018-08-31 11:27:13 -0400

    Fix syntax with core-nix.el

--- core/core-nix.el
+++ core/core-nix.el
@@ -3,6 +3,6 @@
 (setq package-archives
       '(("melpa"        . "http://melpa.org/packages/")
         ("marmalade"    . "http://marmalade-repo.org/packages/")
-        ("org"          . "http://elpa.gnu.org/packages/"))))
+        ("org"          . "http://elpa.gnu.org/packages/")))
 
 (provide 'core-nix)
