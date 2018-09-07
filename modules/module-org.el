d27e67d67768676a8bd4b8f3ddfae2f837327d96
andrunix <andrunix@gmail.com>
2018-09-07 15:54:58 -0400

    Updates for org

--- modules/module-org.el
+++ modules/module-org.el
@@ -73,7 +73,7 @@
 
   ;; Compact the block agenda view
   (setq org-agenda-compact-blocks t)
-
+  
   ;; Custom agenda command definitions
   (setq org-agenda-custom-commands
         (quote (("N" "Notes" tags "NOTE"
@@ -148,6 +148,19 @@
                          (org-tags-match-list-sublevels nil))))
                  nil))))
 
+
+  ;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+  (setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                   (org-agenda-files :maxlevel . 9))))
+
+  ;; Use full outline paths for refile targets - we file directly with IDO
+  ;;(setq org-refile-use-outline-path t)
+
+  ;; Targets complete directly with IDO
+  ;; (setq org-outline-path-complete-in-steps nil)
+
+  ;; Allow refile to create parent tasks with confirmation
+  (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
   )
 
